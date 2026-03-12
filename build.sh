f01699d33f8cc2a490579119f7e9f6e2e1f4f120
U-NISEDUKZ\120606501406 <120606501406@KRG-HB-HOL-WS7.nis.edu.kz>
2026-03-12 16:33:50 +0500

    a

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-wcc main.c -o main \
+gcc main.c -o main \
     -ISDL2/x86/include \
     -LSDL2/x86/lib \
-    -lmingw32 -lSDL2main -lSDL2 -mwindows
+    -lSDL2main -lSDL2 -mwindows
--- main.c
+++ main.c
@@ -2,17 +2,44 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_main.h>
+#include <windows.h>
+#include <math.h>
 
-int main(int,char**){
+SDL_FRect CenterFRect(const SDL_FRect outer, 
+    float iw, float ih)
+{
+    SDL_FRect result;
+    result.w = iw;
+    result.h = ih;
+    result.x = outer.x + (outer.w - iw) / 2.0f;
+    result.y = outer.y + (outer.h - ih) / 2.0f;
+    return result;
+}
+
+WINAPI int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+    LPSTR lpCmdLine, int nShowCmd){
+    SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* window=SDL_CreateWindow("SDL",SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         1000,800,SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     SDL_Renderer* renderer=SDL_CreateRenderer(window,-1,
         SDL_RENDERER_ACCELERATED);
+    SDL_Texture* txt=SDL_CreateTexture(renderer,
+        SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,
+        1000,800);
+    SDL_SetRenderDrawBlendMode(renderer,SDL_BLENDMODE_BLEND);
+    SDL_SetTextureBlendMode(txt,SDL_BLENDMODE_BLEND);
+    SDL_SetRenderTarget(renderer,txt);
+    SDL_SetRenderDrawColor(renderer,0,0,0,0);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderTarget(renderer,NULL);
     bool running=true;
     SDL_FRect plr={0.f,0.f,100.f,100.f};
     int plr_speed=400;
     float dt;
+    bool drawing=false;
+    bool erasing=false;
     int start,end;
     while (running){
         start=SDL_GetTicks();
@@ -24,6 +51,7 @@ int main(int,char**){
                 running=false;
         }
         const Uint8* keys=SDL_GetKeyboardState(NULL);
+        Uint32 mouse=SDL_GetMouseState(NULL,NULL);
         if (keys[SDL_SCANCODE_W]){
             plr.y-=plr_speed*dt;
         }
@@ -36,12 +64,34 @@ int main(int,char**){
         if (keys[SDL_SCANCODE_D]){
             plr.x+=plr_speed*dt;
         }
+        drawing=false;
+        erasing=false;
+        if (mouse & SDL_BUTTON_LMASK) drawing=true;
+        if (mouse & SDL_BUTTON_RMASK) erasing=true;
+
+        SDL_SetRenderTarget(renderer,txt);
+
+        if (drawing){
+            SDL_FRect trail=CenterFRect(plr,10.f,10.f);
+            SDL_SetRenderDrawColor(renderer,0,0,255,255);
+            SDL_RenderFillRectF(renderer,&trail);
+        }
+        if (erasing){
+            SDL_FRect trail=plr;
+            SDL_SetRenderDrawColor(renderer,0,0,0,255);
+            SDL_RenderFillRectF(renderer,&trail);
+        }
+
+        SDL_SetRenderTarget(renderer,NULL);
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
+        SDL_RenderCopy(renderer,txt,NULL,NULL);
+
         SDL_SetRenderDrawColor(renderer,255,0,0,255);
         SDL_RenderFillRectF(renderer,&plr);
+
         SDL_RenderPresent(renderer);
     }
     return 0;
